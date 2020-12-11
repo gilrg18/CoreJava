8d7c301ea6d137f9b79e12c5027664d242592cd3
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2020-12-10 22:59:02 -0700

    Enums

---
+++ Enums/src/Databases.java
@@ -0,0 +1,12 @@
+//ENUM INTRODUCTION
+//every enum class extends the Enum class implicitly
+public enum Databases {
+	// Constants that are objects or references to the class Databases
+	// Implicitly they are public, static and final
+	/*
+	 * Since every Enum class extends the class called Enum from java.lang
+	 * 
+	 * package. It cannot extend any other classes.
+	 */
+	MYSQL, ORACLE, SQLSERVER;
+}
